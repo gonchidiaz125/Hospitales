1c4e3c5ca79b842de372ffa068ee0692449e212d
Kvothejd <jdelmastro@gmail.com>
2023-09-26 16:53:54 -0300

    crear estados de turnos - wip

---
+++ migraciones/20230926-1634-populate estados_turnos.sql
@@ -0,0 +1,11 @@
+  INSERT INTO `hospital`.`estados_turnos` (estado_turno_id, nombre) VALUES (1, 'Libre');
+
+  -- 1 Libre
+  -- 2 Asignado
+  -- 3 Anulado
+  -- 4 Receptado
+  -- 5 Atendiendo
+  -- 6 Atentido
+  -- 7 Ausente
+
+
