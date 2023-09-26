fe352fde5aba48fb1d2c8f67339a04d947c9e037
gonchidiaz125 <gonchi.diaz125@gmail.com>
2023-09-26 17:04:05 -0300

    Agrego todos los estados de turno

--- migraciones/20230926-1634-populate estados_turnos.sql
+++ migraciones/20230926-1634-populate estados_turnos.sql
@@ -8,4 +8,16 @@
   -- 6 Atentido
   -- 7 Ausente
 
+INSERT INTO `hospital`.`estados_turnos` (estado_turno_id, nombre) VALUES (2, 'Asignado');
 
+INSERT INTO `hospital`.`estados_turnos` (estado_turno_id, nombre) VALUES (3, 'Anulado');
+
+INSERT INTO `hospital`.`estados_turnos` (estado_turno_id, nombre) VALUES (4, 'Receptado');
+
+INSERT INTO `hospital`.`estados_turnos` (estado_turno_id, nombre) VALUES (5, 'Atendiendo');
+
+INSERT INTO `hospital`.`estados_turnos` (estado_turno_id, nombre) VALUES (6, 'Atendido');
+
+INSERT INTO `hospital`.`estados_turnos` (estado_turno_id, nombre) VALUES (7, 'Ausente');
+
+SELECT * FROM estados_turnos
